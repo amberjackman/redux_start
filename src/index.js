61096eee5cd3f4f03ace34aac93ed6221a223c8e
amberjackman <awakekaw166@protonmail.ch>
2023-01-04 19:04:00 +0900

    temp

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
+import {addTodo, completeTodo, showComplete} from './redux/actions';
+
+store.subscribe(() => {
+  console.log(store.getState())
+})
+
+store.dispatch(addTodo("할일"))
+store.dispatch(completeTodo(0));
+store.dispatch(showComplete());
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
